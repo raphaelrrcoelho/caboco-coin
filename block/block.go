69638752b08f39b917c75e38f616240c1527f1b4
Raphael Coelho <raphaelrrcoelho@gmail.com>
2018-03-03 00:27:17 -0300

    Refatora blocks

--- block/block.go
+++ block/block.go
@@ -16,7 +16,10 @@ type Block struct {
 
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	headers := bytes.Join(
+		[][]byte{b.PrevBlockHash, b.Data, timestamp},
+		[]byte{},
+	)
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
@@ -25,8 +28,11 @@ func (b *Block) SetHash() {
 // NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{
-		Timestamp: time.Now().Unix(), Data: []byte(data),
-		PrevBlockHash: prevBlockHash,	Hash: []byte{}}
+		Timestamp: time.Now().Unix(),
+		Data: []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash: []byte{},
+	}
 
 	block.SetHash()
 	return block
--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
   bc.AddBlock("Manda mais 2 CBC pro Caboco")
 
   for _, block := range bc.Blocks {
-    fmt.Printf("Previous Hash: %x\n", block.PrevBlockHash)
-    fmt.Printf("Data: %s\n", block.Data)
+    fmt.Printf("Hash Anterior: %x\n", block.PrevBlockHash)
+    fmt.Printf("Dados: %s\n", block.Data)
     fmt.Printf("Hash: %x\n", block.Hash)
     fmt.Println()
   }
